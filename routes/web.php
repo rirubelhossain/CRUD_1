7705787ccfc7693c0d5b92387bbe3d65715d57b1
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-17 14:59:20 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\CrudController ;
 use App\http\Controllers\PostCrd ;
 
 
-Route :: get('/', function(){
-    return view('datainsert');// this is table view page 
-});
-Route :: post('/posts', [CrudController::class, 'datainsert'])->name('datainsert') ;// post route 
-Route :: get('/posts', [CrudController::class,'getdata'])->name('getdata');  
-Route :: get('/view-page/{{id}}',[CrudController::class, 'view_page'])->name('view.page');
-Route :: get('/delete-post/{id}',[CrudController::class, 'deletepost'])->name('delete.post');
 
 
 
